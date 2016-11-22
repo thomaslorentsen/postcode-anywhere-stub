fec2614949b3bbb9d44c291985ad527fe9f824c9
Tom Lorentsen <toml@giffgaff.co.uk>
2016-11-22 11:18:12 +0000

    Add multiple postcode responses

--- main.go
+++ main.go
@@ -87,6 +87,15 @@ func findHandler(w http.ResponseWriter, r *http.Request) {
 			Description: "London, SW1A 1RB",
 		}
 		items.Items = append(items.Items, item);
+
+		item = Item{
+			Id: "GB|RM|A|2",
+			Type: "Address",
+			Text: "The long test",
+			Highlight: "",
+			Description: "A long address",
+		}
+		items.Items = append(items.Items, item);
 	}
 
 	w.Header().Set("Content-Type", "application/json")
@@ -100,22 +109,46 @@ func retrieveHandler(w http.ResponseWriter, r *http.Request) {
 	items := AddressList{
 		Items: make([]Address, 0),
 	}
-	address := Address{
-		Id: "GB|RM|A|1000676865",
-		DomesticId: "1000676865",
-		Language: "ENG",
-		LanguageAlternatives: "ENG",
-		Company: "Ritz Hotel",
-		Street: "Arlington Street",
-		City: "London",
-		Line1: "Arlington Street",
-		PostalCode: "SW1A 1RB",
-		CountryName: "United Kingdom",
-		Label: "Ritz Hotel\nArlington Street\nLONDON\nSW1A 1RB\nUNITED KINGDOM",
-		Type: "Commercial",
-		DataLevel: "Premise",
+	id := r.URL.Query().Get("Id")
+	if (id == "GB|RM|A|2") {
+		address := Address{
+			Id: "GB|RM|A|2",
+			DomesticId: "2",
+			Language: "ENG",
+			LanguageAlternatives: "ENG",
+			Company: "Super Long Widget Company",
+			SubBuilding: "Widget Research Department",
+			BuildingName: "The Big Blue Building",
+			Street: "The Longest Street In The World",
+			Line1: "This is a line that should exceed the limit of fifty characters",
+			Line2: "Line 2",
+			District: "Central District",
+			City: "London",
+			PostalCode: "SW1 1AA",
+			CountryName: "United Kingdom",
+			Label: "This is a really long address",
+			Type: "Commercial",
+			DataLevel: "Premise",
+		}
+		items.Items = append(items.Items, address)
+	} else {
+		address := Address{
+			Id: "GB|RM|A|1000676865",
+			DomesticId: "1000676865",
+			Language: "ENG",
+			LanguageAlternatives: "ENG",
+			Company: "Ritz Hotel",
+			Street: "Arlington Street",
+			Line1: "Arlington Street",
+			City: "London",
+			PostalCode: "SW1A 1RB",
+			CountryName: "United Kingdom",
+			Label: "Ritz Hotel\nArlington Street\nLONDON\nSW1A 1RB\nUNITED KINGDOM",
+			Type: "Commercial",
+			DataLevel: "Premise",
+		}
+		items.Items = append(items.Items, address)
 	}
-	items.Items = append(items.Items, address)
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
